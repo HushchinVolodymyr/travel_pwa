8768faabd3fd4272f5c365d90119067e2c8d54d9
HushchinVolodymyr <114146250+HushchinVolodymyr@users.noreply.github.com>
2025-08-13 17:56:05 +0300

    up4

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
         domains: ['openweathermap.org'],
     },
     eslint: {
-    ignoreDuringBuilds: true,
-  },
+        ignoreDuringBuilds: true,
+    },
 };
 
 module.exports = nextConfig;
